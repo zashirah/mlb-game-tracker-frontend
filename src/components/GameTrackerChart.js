2b384ff918cd33595a3bc140f0d102ed0bc66561
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-10-17 18:32:08 -0400

    built out first version of win % graph. has axis and dots for each atbat and line connecting dots.

--- src/components/GameTrackerChart.js
+++ src/components/GameTrackerChart.js
@@ -1,44 +1,89 @@
 import * as d3 from "d3"
 
-const data = [41, 21, 45, 76, 32]
-
-const url = "https://udemy-react-d3.firebaseio.com/ages.json"
+const MARGIN = { TOP: 10, BOTTOM: 50, LEFT: 70, RIGHT: 10 }
+const WIDTH = 1000 - MARGIN.LEFT - MARGIN.RIGHT
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM
 
 export default class D3Chart {
   constructor(element, gameData) {
-    // console.log(gameData)
-    const svg = d3
+    let vis = this
+
+    // create svg canvas
+    vis.svg = d3
       .select(element)
       .append("svg")
-      .attr("width", '80vw')
-      .attr("height", 500)
+      .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+      .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
 
-    // svg.append("rect")
-    //   .attr("x", 50)
-    //   .attr("y", 50)
-    //   .attr("width", 100)
-    //   .attr("height", 400)
-    //   .attr("fill", "grey");
+    // create y axis
+    vis.y = d3.scaleLinear().domain([0, 1]).range([0, HEIGHT])
 
-    // d3.json(url).then(agesData => {
-      // console.log(gameData)
+    // create x axis
+    vis.x = d3.scaleLinear().domain([0, gameData.length]).range([0, WIDTH])
 
-      const rects = svg.selectAll("rect").data(gameData)
+    // append y axis
+    vis.svg.append("g").attr("class", "y axis").call(d3.axisLeft(vis.y))
 
-      rects
-        .enter()
-        .append("rect")
-        .attr("x", (d, i) => i * 100)
-        .attr("y", 50)
-        .attr("width", 50)
-        .attr("height", (d, i) => d.node.homeTeamWinPct * 100)
-        .attr("fill", d => {
-          if (d.name === "Tony") {
-            return "red"
-          }
-          return "green"
-        })
-    // })
+    // append x axis
+    vis.svg
+      .append("g")
+      .attr("class", "x axis")
+      .call(d3.axisBottom(vis.x))
+      .attr("transform", `translate(0, ${HEIGHT})`)
+
+    // 50% line
+    vis.svg
+      .append("path")
+      .datum([...Array(gameData.length).keys()])
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr(
+        "d",
+        d3
+          .line()
+          .x(function (d) {
+            return vis.x(d)
+          })
+          .y(function (d) {
+            return vis.y(0.5)
+          })
+      )
+
+    // append path, bind data, call generator
+    vis.svg
+      .append("path")
+      .datum(gameData)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr(
+        "d",
+        d3
+          .line()
+          .x(function (d, i) {
+            return vis.x(i)
+          })
+          .y(function (d) {
+            return vis.y(1 - d.node.homeTeamWinPct)
+          })
+      )
+
+    // select dots class, bind data, appendcircles
+    vis.svg
+      .selectAll(".dot")
+      .data(gameData)
+      .enter()
+      .append("circle")
+      .attr("class", "dot")
+      .attr("cx", function (d, i) {
+        return vis.x(i)
+      })
+      .attr("cy", function (d) {
+        return vis.y(1 - d.node.homeTeamWinPct)
+      })
+      .attr("r", 3)
+      .attr("fill", "steelblue")
   }
 
   update() {}
